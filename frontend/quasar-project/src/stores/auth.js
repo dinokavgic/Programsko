dda180d1f0788f2940263d56c423feb7a0ad06a8
Lpesic <lpesic@veleri.hr>
2025-05-14 14:14:54 +0200

    autologout admina 15 min

--- frontend/quasar-project/src/stores/auth.js
+++ frontend/quasar-project/src/stores/auth.js
@@ -1,5 +1,7 @@
 import { defineStore } from 'pinia'
 
+let autoLogoutTimer = null
+
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     user: null,
@@ -13,6 +15,12 @@ export const useAuthStore = defineStore('auth', {
       this.isAdmin = userData.isAdmin || false
 
       localStorage.setItem('authUser', JSON.stringify(userData))
+      if (this.isAdmin) {
+        const loginTime = Date.now()
+        localStorage.setItem('adminLoginTime', loginTime.toString())
+
+        this.setAdminAutoLogout()
+      }
     },
     logout() {
       this.user = null
@@ -20,17 +28,48 @@ export const useAuthStore = defineStore('auth', {
       this.isAdmin = false
 
       localStorage.removeItem('authUser')
+      localStorage.removeItem('adminLoginTime')
+      if (autoLogoutTimer) {
+        clearTimeout(autoLogoutTimer)
+        autoLogoutTimer = null
+      }
     },
     register(userData) {
       // ovdje ide registracija
-      this.login(userData) // user se odmah logira nakon registracije
+      this.login(userData)
     },
     loadUserFromStorage() {
       const storedUser = localStorage.getItem('authUser')
+
       if (storedUser) {
         const userData = JSON.parse(storedUser)
+
+        if (userData.isAdmin) {
+          const loginTime = Number(localStorage.getItem('adminLoginTime'))
+          const now = Date.now()
+          const diff = now - loginTime
+
+          const fifteenMinutes = 15 * 60 * 1000
+          if (diff >= fifteenMinutes) {
+            this.logout()
+
+            return
+          }
+
+          this.setAdminAutoLogout(fifteenMinutes - diff)
+        }
+
         this.login(userData)
       }
+    },
+
+    setAdminAutoLogout(remainingTime = 15 * 60 * 1000) {
+      if (autoLogoutTimer) clearTimeout(autoLogoutTimer)
+
+      autoLogoutTimer = setTimeout(() => {
+        this.logout()
+        alert('Admin sesija je istekla nakon 15 minuta.')
+      }, remainingTime)
     },
   },
 })
